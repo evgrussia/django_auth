477389d0499059713e628eef71663060e1afb4be
Evgussia <evgrussia@gmail.com>
2022-12-24 11:05:31 +0000

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,152 @@
+import datetime
+import random
+import string
+
+from django.core.mail import send_mail
+from rest_framework.authentication import get_authorization_header
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework import exceptions
+from .authentication import create_access_token, create_refresh_token, decode_access_token, decode_refresh_token
+from .models import User, UserToken, Reset
+from .serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Password do not match!')
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=request.data['email']).first()
+
+        if not user:
+            raise exceptions.AuthenticationFailed('Invalid credentials!')
+
+        if not user.check_password(request.data['password']):
+            raise exceptions.APIException('Invalid credentials!')
+
+        access_token = create_access_token(user.id)
+        refresh_token = create_refresh_token(user.id)
+
+        UserToken.objects.create(
+            user_id=user.id,
+            token=refresh_token,
+            expired_at=datetime.datetime.utcnow() + datetime.timedelta(days=7)
+        )
+
+        response = Response()
+
+        response.set_cookie(key='refresh_Token', value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token
+        }
+
+        return response
+
+
+class UserAPIView(APIView):
+    def get(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+
+            user = User.objects.filter(pk=id).first()
+
+            return Response(UserSerializer(user).data)
+
+        raise exceptions.AuthenticationFailed('unauthenticated')
+
+
+class RefreshAPIView(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_Token')
+        id = decode_refresh_token(refresh_token)
+
+        if not UserToken.objects.filter(
+            user_id=id,
+            token=refresh_token,
+            expired_at__gt=datetime.datetime.now(tz=datetime.timezone.utc)
+        ).exists():
+            raise exceptions.AuthenticationFailed('unauthenticated')
+
+        access_token = create_access_token(id)
+
+        return Response({
+            'token': access_token
+        })
+
+
+class LogoutAPIView(APIView):
+    def post(self, request):
+        refresh_token = request.COOKIES.get('refresh_token')
+        UserToken.objects.filter(token=refresh_token).delete()
+
+        response = Response()
+        response.delete_cookie(key="refresh_token")
+        response.data = {
+            'message': 'success'
+        }
+        return response
+
+
+class ForgotAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        token = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+        Reset.objects.create(
+            email=email,
+            token=token
+        )
+
+        url = 'http://localhost:3000/reset/' + token
+
+        send_mail(
+            subject='Reset your Password!',
+            message=f'Click <a href="{url}">here</a> to reset your password',
+            from_email='admin@sotka-test.tk',
+            recipient_list=[email]
+        )
+        return Response({
+            'message': 'success'
+        })
+
+
+class ResetAPIView(APIView):
+    def post(self, request):
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Password do not match!')
+
+        reset_password = Reset.objects.filter(token=data['token']).first()
+
+        if not reset_password:
+            raise exceptions.APIException('Invalid link!')
+
+        user = User.objects.filter(email=reset_password.email).first()
+
+        if not user:
+            raise exceptions.APIException('User not found!')
+
+        user.set_password(data['password'])
+        user.save()
+
+        return Response({
+            'message': 'success'
+        })
